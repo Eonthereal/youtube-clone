049086411fa028c5a4bc74cb4bc845aa731fa9d3
Eonthereal <eon_aris@hotmail.com>
2021-11-21 15:46:00 +0200

    RestControllers are working!!

--- backend/src/main/java/com/exercise/eon/youtubeclone/YoutubeCloneApplication.java
+++ backend/src/main/java/com/exercise/eon/youtubeclone/YoutubeCloneApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.exercise"})
+//@ComponentScan(basePackages = {"com.exercise"})
 public class YoutubeCloneApplication {
 
 	public static void main(String[] args) {
